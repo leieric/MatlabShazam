f08e7414a0de2f636171c47ffd969e3d11eef3c3
Eric Lei <el536@cornell.edu>
2017-11-20 23:37:47 -0500

    Fixed make_database

--- make_database.m
+++ make_database.m
@@ -1,16 +1,26 @@
-function songNameTable = make_database(gs, deltaTL, deltaTU, deltaF)
+function make_database(gs, deltaTL, deltaTU, deltaF)
     Files = what('songDatabase');
     matFiles = Files.mat;
     filename = matFiles{1};
     toRead = ['songDatabase/', filename];
-    songNameTable = []; % 5 column table, applied make_table to all songs
-    
+    songDatabase = []; % 5 column table, applied make_table to all songs
+    songNameTable = [];
     for i = 1:length(matFiles)
         filename = matFiles{i};
         toRead = ['songDatabase/', filename];
         tempMatrix = make_table(toRead, gs, deltaTL, deltaTU, deltaF);
         tempMatrix = [tempMatrix i*ones(length(tempMatrix), 1)];
-        songNameTable = [songNameTable; tempMatrix];
+        songDatabase = [songDatabase; tempMatrix];
+        songName = i;
+        if (songName < 10)
+            songName = strcat(num2str(0), num2str(songName));
+        else
+            songName = num2str(songName);
+        end
+        songName = strcat(songName, '.mat');
+        songNameTable = [songNameTable; songName];
     end
-    size(songNameTable);
+    hashTable = hash(songDatabase);
+    save('hashTable.mat', 'hashTable', '-mat');
+    save('songNameTable.mat', 'songNameTable', '-mat');
 end
